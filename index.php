5288307b15929f9e8b28ae7c82037cff3e1b2c5b
nemat ghaznavi <nemat.ghaznavi313@gmail.com>
2021-03-01 11:39:22 +0330

    add initial file and create home page

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="fa" dir="rtl">
+<head>
+    <title>Nemat Ghaznavi</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="/image/fav_icon.png" type="image/gif" sizes="16x16">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <div>
+        <ul class="navmenu">
+            <li><a>خانه</a></li>
+            <li><a>درباره من</a></li>
+            <li><a>خدمات</a></li>
+            <li><a>رزومه</a></li>
+            <li><a>نمونه کارها</a></li>
+            <li><a>ارتباط با من</a></li>
+        </ul>
+    </div>
+</header>
+
+<main>
+    <div class="me">
+        <div style="margin:20% 15% 0px 0px;">
+            <span style="text-align: center; margin-right: 25px;">سلام، من <b style="color: chocolate; font-size: 26px">نعمت هستم</b></span><br><br>
+            <span style="font-size: 30px">توسعه دهنده وب سایت</span><br><br><br>
+            <a href="/image/me.jpg" style="background-color: chocolate">دانلود رزومه</a>
+            <a href="#" style="background-color: darkcyan">درباره من</a>
+        </div>
+    </div>
+</main>
+
+<footer></footer>
+
+</body>
+</html>
