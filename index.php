df59b6b5ae3d3b7295566b015352e27b76c7adc0
nemat ghaznavi <ne.ghaznavi@gmail.com>
2021-05-28 02:49:04 +0430

    hero section were create

--- index.php
+++ index.php
@@ -9,23 +9,47 @@
     <link rel="shortcut icon"href="images/favicon.ico" type="image/x-icon">
     <!-- add custome style -->
     <link rel="stylesheet" href="styles/main.css"/>
+    <!-- awesome icon -->
+    <link rel="stylesheet" href="styles/all.css"/>
 </head>
 <body>
     <div class="container">
+        <!-- navigation section -->
         <nav class="navigation">
             <ul class="menu-list">
-                <li class="menu-item"><a href="#">خانه</a></li>
+                <li class="menu-item"><a href="#hero">خانه</a></li>
                 <li class="menu-item"><a href="#">وبلاگ</a></li>
-                <li class="menu-item"><a href="#">رزومه</a></li>
                 <li class="menu-item"><a href="#">مهارت ها</a></li>
                 <li class="menu-item"><a href="#">نمونه کارها</a></li>
-                <li class="menu-item"><a href="#">درباره من</a></li>
                 <li class="menu-item"><a href="#">تماس با من</a></li>
             </ul>
             <div class="brand">
                 <p><span>D</span>eveloper<span>01</span></p>
             </div>
+        
+        
+        
+        
         </nav>
+
+        <!-- hero section -->
+        <div class="hero" id="hero">
+            <img src="images/model.png">
+            <div class="bio">
+                <h2>سلام</h2>
+                <h1>من، نعمتم</h1>
+                <h3>برنامه نویس وب سایت</h3>
+                <a href="#" class="btn-resume">دانلود رزومه</a>
+            </div>
+            <ul class="header_social d-none d-lg-block">
+                <li><a href="#"><i class="fab fa-github"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fab fa-telegram"></i></a></li>
+                <li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+            </ul>
+        </div>
     </div>
 
     <!-- add scripts -->
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const nav = document.querySelector('.navigation');
 
 window.onscroll = function() {
     let top = window.scrollY;
-    if (top > 150) {
+    if (top > 100) {
         nav.classList.add('navScrolled');
     } else {
         nav.classList.remove('navScrolled');
